57fc0c0a17c47d257a6670e1c7fcb7ee090060d2
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-21 16:52:59 +0530

    Middleware for loginPage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
     <div>
       <h1>HI</h1>
       <ThemeToggle />
+      {session ? <p>{session.user.name}</p> : <Button>Login</Button>}
     </div>
   );
 }
