0f5f873af613ba801b78bb455aa7822674399b22
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-25 15:11:02 +0530

    Implemented rate limiting.

--- app/api/s3/upload/route.ts
+++ app/api/s3/upload/route.ts
@@ -4,6 +4,13 @@ import { v4 as uuidv4 } from "uuid";
 import { NextResponse } from "next/server";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+const aj = arcjet
+  .withRule(detectBot({ mode: "LIVE", allow: [] }))
+  .withRule(fixedWindow({ mode: "LIVE", window: "1m", max: 5 }));
 
 export const fileUploadSchema = z.object({
   fileName: z.string().min(1, { message: "Filename is required," }),
@@ -13,7 +20,17 @@ export const fileUploadSchema = z.object({
 });
 
 export async function POST(request: Request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   try {
+    const decision = await aj.protect(request, {
+      fingerprint: session?.user.id as string,
+    });
+
+    if (decision.isDenied()) {
+      return NextResponse.json({ error: "Errrrrrrrrrrrr" }, { status: 429 });
+    }
     const body = await request.json();
 
     const validation = fileUploadSchema.safeParse(body);
