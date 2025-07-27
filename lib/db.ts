810955f719bd0f0d800c0186f12252bf6395a53c
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-27 15:58:15 +0530

    Prisma instance for production updated.

--- lib/db.ts
+++ lib/db.ts
@@ -3,4 +3,8 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// For long lived servers
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// For serverless production
+globalForPrisma.prisma = prisma;
