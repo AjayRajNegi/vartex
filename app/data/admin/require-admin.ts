d7e58465e048a1c68978654d388e76008ac9641d
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-28 23:37:18 +0530

    require-admin.ts updated

--- app/data/admin/require-admin.ts
+++ app/data/admin/require-admin.ts
@@ -1,18 +1,22 @@
 import "server-only";
+
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+// Asynchronous function to enforce admin-only access
 export async function requireAdmin() {
+  // Attempt to get the user's session from the authentication API, using the request headers
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
   if (!session) {
     return redirect("/login");
   }
+
   if (session.user.role !== "admin") {
     return redirect("/not-admin");
   }
+
   return session;
 }
