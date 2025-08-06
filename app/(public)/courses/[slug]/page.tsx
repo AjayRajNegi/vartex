936533beea0fab8db551c23e51c21479f393c883
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-08-06 12:15:53 +0530

    Individual course page completed.

--- app/(public)/courses/[slug]/page.tsx
+++ app/(public)/courses/[slug]/page.tsx
@@ -1,21 +1,25 @@
-import { getIndividualCourse } from "@/app/data/course/get-course";
-import { RenderDescription } from "@/components/rich-text-editor/RenderDesctiption";
-import { Badge } from "@/components/ui/badge";
-import { Card, CardContent } from "@/components/ui/card";
+import {
+  IconBook,
+  IconClock,
+  IconCategory,
+  IconChartBar,
+  IconPlayerPlay,
+  IconChevronDown,
+} from "@tabler/icons-react";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { Separator } from "@/components/ui/separator";
-import {
-  IconCategory,
-  IconChartBar,
-  IconChevronDown,
-  IconClock,
-  IconPlayerPlay,
-} from "@tabler/icons-react";
+
 import Image from "next/image";
+import { CheckIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Card, CardContent } from "@/components/ui/card";
+import { getIndividualCourse } from "@/app/data/course/get-course";
+import { RenderDescription } from "@/components/rich-text-editor/RenderDesctiption";
 
 type Params = Promise<{ slug: string }>;
 
@@ -27,7 +31,7 @@ export default async function IndividualCoursePage({
   const { slug } = await params;
   const course = await getIndividualCourse(slug);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5 gap-5">
       <div className="order-1 lg:col-span-2">
         <div className="relative aspect-video w-full overflow-hidden rounded-xl shadow-lg">
           <Image
@@ -148,6 +152,116 @@ export default async function IndividualCoursePage({
               </Card>
             </Collapsible>
           ))}
+        </div>
+      </div>
+
+      {/* Enrollment Card */}
+      <div className="order-2 lg:col-span-1">
+        <div className="sticky top-20">
+          <Card className="py-0">
+            <CardContent className="p-6">
+              <div className="flex items-center justify-between mb-6">
+                <span className="text-lg font-medium">Price:</span>
+                <span className="text-2xl font-bold text-primary">
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "INR",
+                  }).format(course.price)}
+                </span>
+              </div>
+
+              <div className="mb-6 space-y-3 rounded-lg bg-muted p-4">
+                <h4 className="font-medium">What you will get:</h4>
+                <div className="flex flex-col gap-3">
+                  <div className="flex items-center gap-3">
+                    <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                      <IconClock className="size-4" />
+                    </div>
+                    <div>
+                      <p className="text-sm font-medium">Course Duration</p>
+                      <p className="text-sm text-muted-foreground">
+                        {course.duration} hours
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-3">
+                  <div className="flex items-center gap-3">
+                    <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                      <IconChartBar className="size-4" />
+                    </div>
+                    <div>
+                      <p className="text-sm font-medium">Difficulty Level</p>
+                      <p className="text-sm text-muted-foreground">
+                        {course.level}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-3">
+                  <div className="flex items-center gap-3">
+                    <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                      <IconCategory className="size-4" />
+                    </div>
+                    <div>
+                      <p className="text-sm font-medium">Category</p>
+                      <p className="text-sm text-muted-foreground">
+                        {course.category}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-3">
+                  <div className="flex items-center gap-3">
+                    <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                      <IconBook className="size-4" />
+                    </div>
+                    <div>
+                      <p className="text-sm font-medium">Total Lessons</p>
+                      <p className="text-sm text-muted-foreground">
+                        {course.chapter.reduce(
+                          (total, chapter) => total + chapter.lessons.length,
+                          0
+                        ) || 0}{" "}
+                        Lessons
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-6 space-y-3">
+                <h4>This course includes:</h4>
+                <ul className="space-y-2">
+                  <li className="flex items-center gap-2 text-sm">
+                    <div className="rounded-full p-1 bg-green-500/10 text-green-500">
+                      <CheckIcon className="size-3" />
+                    </div>
+                    <span>Full lifetime access</span>
+                  </li>
+                  <li className="flex items-center gap-2 text-sm">
+                    <div className="rounded-full p-1 bg-green-500/10 text-green-500">
+                      <CheckIcon className="size-3" />
+                    </div>
+                    <span>Access on mobile and desktop.</span>
+                  </li>
+                  <li className="flex items-center gap-2 text-sm">
+                    <div className="rounded-full p-1 bg-green-500/10 text-green-500">
+                      <CheckIcon className="size-3" />
+                    </div>
+                    <span>Certificate of completion.</span>
+                  </li>
+                </ul>
+              </div>
+
+              <Button className="w-full">Enroll Now!</Button>
+              <p className="mt-3 text-center text-xs text-muted-foreground ">
+                30-day money-back guarantee
+              </p>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
