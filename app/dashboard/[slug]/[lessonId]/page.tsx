217cd024e47ab80264af568c0e6eb7971e41f3ba
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-08-08 23:14:11 +0530

    Video player for the lessons.

---
+++ app/dashboard/[slug]/[lessonId]/page.tsx
@@ -0,0 +1,14 @@
+import { CourseContent } from "./_components/CourseContent";
+import { getLessonContent } from "@/app/data/course/get-lesson-content";
+
+type Params = Promise<{ lessonId: string }>;
+
+export default async function LessonContentPage({
+  params,
+}: {
+  params: Params;
+}) {
+  const { lessonId } = await params;
+  const data = await getLessonContent(lessonId);
+  return <CourseContent data={data} />;
+}
