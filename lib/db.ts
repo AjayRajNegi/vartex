b0e398c3caa7a52aa0bec364c0276fcaa8b9dada
Ajay <ajayrajnegi111@gmail.com>
2025-09-26 15:26:32 +0530

    schema update

--- lib/db.ts
+++ lib/db.ts
@@ -3,8 +3,9 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
+globalForPrisma.prisma = prisma;
+
 // For long lived servers
 // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 // For serverless production
-globalForPrisma.prisma = prisma;
